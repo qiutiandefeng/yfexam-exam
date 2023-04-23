3f4a8f383b71658b74ccd7171d86ad0fe61cf67a
云帆互联 <xiaoquan328@163.com>
2023-04-23 15:18:57 +0800

    Create MybatisConfig.java
    更新config文件
    
    Signed-off-by: 云帆互联 <xiaoquan328@163.com>

--- exam-api/src/main/java/com/yf/exam/config/MybatisConfig.java
+++ exam-api/src/main/java/com/yf/exam/config/MybatisConfig.java
@@ -1,11 +1,9 @@
 package com.yf.exam.config;
-
 import com.yf.exam.aspect.mybatis.QueryInterceptor;
 import com.yf.exam.aspect.mybatis.UpdateInterceptor;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 /**
  * Mybatis过滤器配置
  * 注意：必须按顺序进行配置，否则容易出现业务异常
