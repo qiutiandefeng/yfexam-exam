48999377e84c98a43c1296c97553892aa7d5676f
云帆互联 <xiaoquan328@163.com>
2023-05-12 09:44:05 +0800

    Update user.js
    更新
    
    Signed-off-by: 云帆互联 <xiaoquan328@163.com>

--- exam-vue/src/store/modules/user.js
+++ exam-vue/src/store/modules/user.js
@@ -1,7 +1,6 @@
 import { login, reg, logout, getInfo } from '@/api/user'
 import { getToken, setToken, removeToken } from '@/utils/auth'
 import router, { resetRouter } from '@/router'
-
 const state = {
   token: getToken(),
   userId: '',
