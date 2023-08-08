c2f0ab23749aab57531aa8554ee389678849de41
云帆互联 <xiaoquan328@163.com>
2023-08-09 06:29:23 +0800

    Update ExamController.java
    提交更新
    
    Signed-off-by: 云帆互联 <xiaoquan328@163.com>

--- exam-api/src/main/java/com/yf/exam/modules/exam/controller/ExamController.java
+++ exam-api/src/main/java/com/yf/exam/modules/exam/controller/ExamController.java
@@ -20,7 +20,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
 import java.util.Date;
 
 /**
